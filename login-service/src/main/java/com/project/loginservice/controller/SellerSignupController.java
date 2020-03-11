87deb6c25555ef56d1c8c14482f7c32d22c0f738
gayathri5252 <60173523+gayathri5252@users.noreply.github.com>
2020-03-11 18:12:42 +0530

    Add files via upload

---
+++ login-service/src/main/java/com/project/loginservice/controller/SellerSignupController.java
@@ -0,0 +1,42 @@
+package com.project.loginservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.loginservice.pojo.BuyerSignupPojo;
+import com.project.loginservice.pojo.SellerSignupPojo;
+import com.project.loginservice.service.BuyerSignupService;
+import com.project.loginservice.service.SellerSignupService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("emart")
+public class SellerSignupController {
+	@Autowired
+	SellerSignupService sellerSignupService;
+
+	// EndPoint to validate Buyer
+	@GetMapping("validateseller")
+	SellerSignupPojo validateSeller(@RequestHeader("Authorization") String data) {
+		
+		String token[] = data.split(":");
+		SellerSignupPojo sellerSignupPojo = new SellerSignupPojo();
+		sellerSignupPojo.setUsername(token[0]);
+		sellerSignupPojo.setPassword(token[1]);
+		return sellerSignupService.validateSeller(sellerSignupPojo);
+		
+
+	}
+	
+
+	@GetMapping("/seller/{sellerId}")
+SellerSignupPojo getSellerItem(@PathVariable("sellerId") Integer sellerId)
+{
+	return sellerSignupService.getItems(sellerId);
+}
+}
